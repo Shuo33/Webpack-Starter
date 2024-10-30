a0948b2a7060d85a7f6746feaf47d03ede151af9
shuo xu <shuo.xu@2021.icam.fr>
2024-10-30 14:41:26 +0100

    webpack basic setup

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        //allow us to use the absolute file path
+        // __dirname means current directory
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    }
+};
